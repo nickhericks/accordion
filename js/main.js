c88b97433e9f5a28a78cac4ac26dd97ae73e6880
Nick-Hericks <nickhericks@gmail.com>
2019-02-19 11:22:27 +0200

    Refactor class selection for event listener

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
+// Select all nodes that have the class of '.accordion'
 const accordions = document.querySelectorAll('.accordion');
 
 // Create event listener for each accordion node
 accordions.forEach((accordion, index) => {
-
+  // Toggle 'is-open' class when accordion node is clicked
   accordion.addEventListener('click', function() {
     accordion.classList.toggle('is-open');
-    console.log(accordion.classList());
   });
-
-
 });
