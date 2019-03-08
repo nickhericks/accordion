406061f9d0f4e0aa1e66cb0396db488090da7995
Nick-Hericks <nickhericks@gmail.com>
2019-03-08 09:17:18 +0200

    Refactor accordion js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
 // Container for all accordion elements
-const container = document.querySelector('.accordion-container');
+const container = document.querySelector('.accordion-container')
 
-
-// Event listener on the entire accordion-container element
 container.addEventListener('click', e => {
-	// Find closest bubbling element that has a class of accordion__header
-	const header = e.target.closest('.accordion__header');
-	// If that element exists in the event bubbling of the click, toggle is-open class
-	if (header) {
-		const accordion = header.parentElement;
-		const content = header.nextElementSibling;
-		const inner = content.children[0];
-		const height = inner.getBoundingClientRect().height;
+	const header = e.target.closest('.accordion__header')
+	if (!header) return
+	const accordion = header.parentElement
+	const content = header.nextElementSibling
+	const inner = content.children[0]
+	const height = accordion.classList.contains('is-open')
+		? 0
+		: inner.getBoundingClientRect().height
 
-		if (accordion.classList.contains('is-open')) {
-			content.style.height = '0px';
-		} else {
-			content.style.height = height + 'px';
-		}
-
-		accordion.classList.toggle('is-open');
-	}
-});
+	content.style.height = `${height}px`
+	accordion.classList.toggle('is-open')
+})
