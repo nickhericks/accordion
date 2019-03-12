1ace0c7755665296b571584ba9f4beb43c2e1c22
Nick-Hericks <nickhericks@gmail.com>
2019-03-12 12:46:51 +0200

    Create declarative functions

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-// Container for all accordion elements
-const container = document.querySelector('.accordion-container')
 
-container.addEventListener('click', e => {
-	const header = e.target.closest('.accordion__header')
-	if (!header) return
-	const accordion = header.parentElement
-	const content = header.nextElementSibling
-	const inner = content.children[0]
+const getHeight = (accordion, content) => {
+	const inner = content.children[0];
 	const height = accordion.classList.contains('is-open')
 		? 0
-		: inner.getBoundingClientRect().height
+		: inner.getBoundingClientRect().height;
 
-	content.style.height = `${height}px`
-	accordion.classList.toggle('is-open')
-})
+	return height;
+};
+
+const toggleAccordion = accordion => {
+	accordion.classList.toggle('is-open');
+};
+
+const updateHeight = (elem, height) => {
+	elem.style.height = `${height}px`;
+};
+
+// Container for all accordion elements
+const container = document.querySelector('.accordion-container');
+
+container.addEventListener('click', e => {
+	const header = e.target.closest('.accordion__header');
+	if (!header) return;
+
+	const accordion = header.parentElement;
+	const content = header.nextElementSibling;
+	const height = getHeight(accordion, content);
+
+	updateHeight(content, height);
+	toggleAccordion(accordion);
+});
